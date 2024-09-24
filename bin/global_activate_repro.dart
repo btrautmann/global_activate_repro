88f57b3b4dcbb02ab3b539855fac47becbd28bd9
Brandon Trautmann <btrautmann@users.noreply.github.com>
2024-09-24 09:33:48 -0400

    initial commit

---
+++ bin/global_activate_repro.dart
@@ -0,0 +1,5 @@
+import 'package:global_activate_repro/global_activate_repro.dart' as global_activate_repro;
+
+void main(List<String> arguments) {
+  print('Hello world: ${global_activate_repro.calculate()}!');
+}
---
+++ test/global_activate_repro_test.dart
@@ -0,0 +1,8 @@
+import 'package:global_activate_repro/global_activate_repro.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
